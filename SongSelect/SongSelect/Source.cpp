af9575ad2c1ec3129cb2080a72065f7a99af1e3e
game110pm <game110pm@605-110-STU03>
2017-02-02 13:02:36 -0700

    JUKEBOX BOYO

---
+++ SongSelect/SongSelect/Source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int choice;
+
+	cout << "What song do you want" << endl;
+	cin >> choice;
+
+	switch (choice) {
+	case 1:
+		cout << "You got the lights on in the afternoon" << endl
+			<< "And the nights are drawn out long" << endl
+			<< "And you're kissing to cut through the gloom" << endl
+			<< " With a cough drop coloured tongue" << endl;
+		break;
+
+	case 2:
+		cout << "Accept the fact that everything you hate" << endl
+			<< "Your thoughts, your words and every single day" << endl
+			<< "Defines you, defined you" << endl
+			<< "(Ooh, oh, oh)" << endl;
+		break;
+	case 3:
+		cout << "You don’t know the name of like half of these folks" << endl
+			<< "And they’ll be sprinting for the exit the minute you’re broke" << endl
+			<< "A bunch of bobblehead muppets to laugh at your jokes" << endl
+			<< "Wipe your ass and come and give your ego a stroke" << endl;
+		break;
+
+
+
+
+	}
+}
